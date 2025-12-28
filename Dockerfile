c3b7e7fdd99b44db77536fa8f692095559f781ae
Julien.Prados <julien.prados@unige.ch>
2025-12-28 08:59:50 +0100

    reorganize site in docs

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM ruby:trixie
 
 RUN apt update && apt install -y build-essential zlib1g-dev
 RUN gem install jekyll bundler
+#RUN bundle install minima jekyll-feed jekyll-seo-tag
 
+EXPOSE 4040
 VOLUME /cwd
 WORKDIR /cwd
--- jekyll.sh
+++ jekyll.sh
@@ -1,11 +1,20 @@
 
 docker build -t jekyll ./
 
-docker run --rm -it -v="./:/cwd" \
+docker run --rm -it -v "./:/cwd" -p "4000:4000" \
   jekyll \
-  jekyll new www
-  
-  
-jekyll build www
-  
+  bash
+
+# Create new site
+jekyll new docs
+cd docs
+bundle add webrick logger
+
+
+# display
+cd docs
+bundle install
+bundle exec jekyll serve
+
+
   
